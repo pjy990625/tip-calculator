af1f2d478147e8adfe49cabf85715c003a54e908
Skylar Park <90671077+pjy990625@users.noreply.github.com>
2024-01-28 12:36:04 -0800

    added media queries for mobile

--- tip-calculator/src/components/Results.jsx
+++ tip-calculator/src/components/Results.jsx
@@ -17,50 +17,52 @@ function Results({ range, results, onBackBtnClick, onRestartBtnClick }) {
     }
 
     return (
-        <div className="wrapper">
-            <button className='button' onClick={onBackBtnClick}>Back</button>
-            <div className="results-container">
-                {summaryBtnClicked ?
-                    <Summary range={range} results={results} /> :
-                    <table className="table">
-                        {sortedResults.map((result, key) => {
-                            const currentDate = `${result.date}`;
-                            const renderThead = currentDate !== prevDate;
-                            prevDate = currentDate;
+        <div className='mobile-wrapper'>
+            <div className="wrapper">
+                <button className='button' onClick={onBackBtnClick}>Back</button>
+                <div className="results-container">
+                    {summaryBtnClicked ?
+                        <Summary range={range} results={results} /> :
+                        <table className="table">
+                            {sortedResults.map((result, key) => {
+                                const currentDate = `${result.date}`;
+                                const renderThead = currentDate !== prevDate;
+                                prevDate = currentDate;
 
-                            return (
-                                <React.Fragment key={key} >
-                                    {renderThead &&
-                                        <thead key={`thead-${currentDate}`}>
-                                            <tr className='table-highlight'>
-                                                <th>{monthNum}/{result.date}</th>
-                                                <th>Morning Tip</th>
-                                                <th>Evening Tip</th>
-                                                <th>Total Tip</th>
+                                return (
+                                    <React.Fragment key={key} >
+                                        {renderThead &&
+                                            <thead key={`thead-${currentDate}`}>
+                                                <tr className='table-highlight'>
+                                                    <th>{monthNum}/{result.date}</th>
+                                                    <th>Morning Tip</th>
+                                                    <th>Evening Tip</th>
+                                                    <th>Total Tip</th>
+                                                </tr>
+                                            </thead>}
+                                        <tbody key={`tbody-${currentDate}`}>
+                                            <tr>
+                                                <th>{result.name}</th>
+                                                <th className='number'>{result.morningTip}</th>
+                                                <th className='number'>{result.eveningTip}</th>
+                                                <th className='number'>{result.morningTip + result.eveningTip}</th>
                                             </tr>
-                                        </thead>}
-                                    <tbody key={`tbody-${currentDate}`}>
-                                        <tr>
-                                            <th>{result.name}</th>
-                                            <th className='number'>{result.morningTip}</th>
-                                            <th className='number'>{result.eveningTip}</th>
-                                            <th className='number'>{result.morningTip + result.eveningTip}</th>
-                                        </tr>
-                                    </tbody>
-                                </React.Fragment>
-                            )
-                        })}
-                    </table>
-                }
-            </div>
-            <div>
-                {summaryBtnClicked ?
-                    <button className='button' onClick={handleSummaryBtnClicked}>Table</button> :
-                    <button className='button' onClick={handleSummaryBtnClicked}>Summary</button>
-                }
-                <button className='button' onClick={onRestartBtnClick}>Restart</button>
-            </div>
-        </div >
+                                        </tbody>
+                                    </React.Fragment>
+                                )
+                            })}
+                        </table>
+                    }
+                </div>
+                <div>
+                    {summaryBtnClicked ?
+                        <button className='button' onClick={handleSummaryBtnClicked}>Table</button> :
+                        <button className='button' onClick={handleSummaryBtnClicked}>Summary</button>
+                    }
+                    <button className='button' onClick={onRestartBtnClick}>Restart</button>
+                </div>
+            </div >
+        </div>
     );
 }
 
